a8b4df803a032ee54bb4d72237c14ebcaa22c9a4
Perry <thescratcherman@gmail.com>
2015-05-22 11:55:22 -0400

    "Added code to initiate handshake with groupme push server"

--- botDetector.py
+++ botDetector.py
@@ -4,6 +4,22 @@ import json
 
 GROUPME_API = "https://api.groupme.com/v3"
 
+# create handshake with groupme server to recieve new messages
+def initiate_connection():
+    # data required by groupme for initial handshake
+    handshake_data = json.dumps([
+                        {
+                            "channel":"/meta/handshake",
+                            "version":"1.0",
+                            "supportedConnectionTypes":["long-polling"],
+                            "id":"1"
+                        }])
+    # encode data into bytes and send to groupme
+    data = handshake_data.encode('utf-8')
+    request = urllib.request.Request("https://push.groupme.com/faye",
+            data, {"Content-Type":"application/json"})
+    response = urllib.request.urlopen(request)
+
 # fetches resource at URL, converts JSON to useful Object
 def make_request(base_url, additional_url, token):
 
